4d6bd99cefe61ddb0f4221e85296e6d0a8134457
Lucas Zorzi <lucas.zorzi@hotmail.com>
2021-08-04 01:44:24 -0300

    Adding conversor and homepage

---
+++ README.md
@@ -0,0 +1,3 @@
+This project is a React app.
+
+It works to generate references in ABNT's pattern.
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Conversor from './components/conversor'
+
+function App() {
+  return (
+    <Conversor moedaA="USD" moedaB="BRL" />
+  );
+}
+
+export default App;
